1a84644bab29933c885288a8089167dfc85053bc
destajador <destajador57@hotmail.com>
2017-12-07 10:35:00 -0600

    Cambiar la direccion de los servicios

--- miAuto/src/providers/user-service/user-service.ts
+++ miAuto/src/providers/user-service/user-service.ts
@@ -10,31 +10,31 @@ export class UserServiceProvider {
 
   //
   Loguea(user,psw){
-	return this.http.get('http://localhost:4800/Logea?user='+user+'&password='+psw);
+	return this.http.get('http://192.168.20.9:4800/Logea?user='+user+'&password='+psw);
   }
   //
   Registra(user,psw,vin,placas){
-	return this.http.get('http://localhost:4800/Registra?user='+user+'&password='+psw+'&vin='+vin+'&placas='+placas);
+	return this.http.get('http://192.168.20.9:4800/Registra?user='+user+'&password='+psw+'&vin='+vin+'&placas='+placas);
   }
   //
   GetUnidades(idUser){
-	return this.http.get('http://localhost:4800/GetUnidades?idUser='+idUser);
+	return this.http.get('http://192.168.20.9:4800/GetUnidades?idUser='+idUser);
   }
   //
   GetTalleres(idUnidad,lat,lng){
-	return this.http.get('http://localhost:4800/GetTalleres?idUnidad='+idUnidad+'&lat='+lat+'&lng='+lng);
+	return this.http.get('http://192.168.20.9:4800/GetTalleres?idUnidad='+idUnidad+'&lat='+lat+'&lng='+lng);
   }
   //
   GetOrdXUni(idUnidad){
-	return this.http.get('http://localhost:4800/GetOrdXUni?idUnidad='+idUnidad);
+	return this.http.get('http://192.168.20.9:4800/GetOrdXUni?idUnidad='+idUnidad);
   }
   //
   GetDocsXUni(idUnidad){
-	return this.http.get('http://localhost:4800/GetDocsXUni?idUnidad='+idUnidad);
+	return this.http.get('http://192.168.20.9:4800/GetDocsXUni?idUnidad='+idUnidad);
   }
   
   ServicioNuevaCita(idUnidad,idContratoOperacion,idUsuario,Taller,servicio,fechaCita){
-	return this.http.get('http://localhost:4800/ServicioNuevaCita?idUnidad=' + idUnidad + 
+	return this.http.get('http://192.168.20.9:4800/ServicioNuevaCita?idUnidad=' + idUnidad + 
 							'&idContratoOperacion=' + idContratoOperacion +
 							'&idUsuario=' + idUsuario +
 							'&idTaller=' + Taller +
@@ -44,11 +44,11 @@ export class UserServiceProvider {
   }
 
   CitaServicios(){
-    return this.http.get('http://localhost:4800/CitaServicios');
+    return this.http.get('http://192.168.20.9:4800/CitaServicios');
     }
   
     CalificaTaller(idOrden,calificacion){
-      return this.http.get('http://localhost:4800/CalificaTaller?idOrden=' + idOrden + 
+      return this.http.get('http://192.168.20.9:4800/CalificaTaller?idOrden=' + idOrden + 
                   '&calificacion1=' + calificacion.primera +
                   '&calificacion2=' + calificacion.segunda +
                   '&calificacion3=' + calificacion.tercera +
